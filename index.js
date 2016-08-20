744404468e0c48113de06e820c25e04b8f5c345c
areshytko <alex4am4i@gmail.com>
2016-08-20 23:52:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by areshytko on 08.08.16.
+ */
+
+
+requirejs.config({
+    baseUrl: '/webrtctest/',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'node_modules/jquery/dist/jquery',
+        main: 'main'
+    }
+});
+
+
+requirejs(['main']);
---
+++ main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by areshytko on 08.08.16.
+ */
+
+define(['jquery'], function ($){
+    
+});
